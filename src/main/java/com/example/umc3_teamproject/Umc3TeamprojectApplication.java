7a11cf6df319b9a24ab062ab900efddab93cb29d
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-12-31 11:59:04 +0900

    first commit

---
+++ src/main/java/com/example/umc3_teamproject/Umc3TeamprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.umc3_teamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc3TeamprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc3TeamprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/umc3_teamproject/Umc3TeamprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc3_teamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc3TeamprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
