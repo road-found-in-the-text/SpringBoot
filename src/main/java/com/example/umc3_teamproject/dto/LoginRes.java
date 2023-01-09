1b9d3001a80e0a3ef30b811a4c6dcfbee517a521
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2023-01-10 00:08:42 +0900

    [feat] #4 jwt 구현

--- src/main/java/com/example/umc3_teamproject/dto/LoginRes.java
+++ src/main/java/com/example/umc3_teamproject/dto/LoginRes.java
@@ -2,18 +2,17 @@ package com.example.umc3_teamproject.dto;
 
 import com.example.umc3_teamproject.domain.LoginType;
 import com.example.umc3_teamproject.domain.Tier;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Getter // 해당 클래스에 대한 접근자 생성
+@Setter // 해당 클래스에 대한 설정자 생성
+@AllArgsConstructor // 해당 클래스의 모든 멤버 변수(userIdx, jwt)를 받는 생성자를 생성
 public class LoginRes {
-    private Long id;
-    private String nickName;
-    private String imageUrl;
-    private Tier tier;
-    private LoginType loginType;
 
-    //허용
-    private boolean comments_alarm_permission;
-    private boolean voice_permission;
-    private boolean event_permission;
-    private boolean report_status;
+    private int memberId;
+    private String jwt;
 
 }
